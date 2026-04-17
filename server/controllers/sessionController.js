0ba3883ffb0700fee0d447187aea84b9ce4dd7e4
Valoree Armstrong <armstrongvaloree@gmail.com>
2026-04-17 15:18:18 -0400

    fix: replace bcrypt with bcryptjs for Node 22 compatibility

--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -1,5 +1,5 @@
 const { v4: uuidv4 } = require('uuid');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Session = require('../models/Session');
 const User = require('../models/User');
 
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 
